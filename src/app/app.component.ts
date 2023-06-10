c43b1dbfc2922353cb70198066441aad3cac6301
Davood <da.mirzaee@gmail.com>
2023-06-10 16:07:48 +0330

    changing RXJS of this project and ending it. It work fine.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,16 +1,17 @@
 import { Component } from '@angular/core';
 import { WikipediaService } from './wikipedia.service';
+
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
-  pages = [];
+  pages: any = [];
   constructor(private wikipedia: WikipediaService) {}
   onTerm(term: string) {
-    this.wikipedia.search(term).subscribe((response: any) => {
-      this.pages = response.query.search;
+    this.wikipedia.search(term).subscribe((pages) => {
+      this.pages = pages;
     });
   }
 }
